6640bfc31e0f1ebaf35d30874b83abf7f185d9b5
MukhtarOsmani <mukhtar.a.osmani@gmail.com>
2022-05-23 16:51:57 -0400

    first commit

---
+++ js/myScript.js
@@ -0,0 +1,13 @@
+function showMessage()
+        {
+            // document.getElementById('message').innerHTML = 'WELCOME TO OUR WEBSITE';
+            // document.getElementById('message').style.cssText =
+            //  "border:solid green 2px;" width:250px; height:200px";
+            // document.querySelector('div').style.cssText = 'boder:solid red 2px; width:250px; height:200'
+           
+        }
+
+        const names = ["ahmad", "samad", "rahman","massoud","Ahmad Tahir","wahid"];
+        const class2 = ["John", "Alex", "Chris","Kavin"];
+
+     
